c0b25c57d4310d0ce10084557fc7eba0cf6d5d3f
xiaoxunyao <huchengyea@163.com>
2022-04-28 17:33:05 +0800

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+
+import path from 'path'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  }
+})
