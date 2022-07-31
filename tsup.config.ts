e67758be74ee0a45fcb3e5677f8fb89161b4b4cf
xiaoxunyao <huchengyea@163.com>
2022-07-31 09:01:01 +0800

    chore: init build config

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+  ],
+  dts: true,
+  format: [
+    'cjs',
+    'esm',
+  ],
+  clean: true,
+})
